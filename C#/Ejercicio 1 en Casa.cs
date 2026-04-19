f03e526864b652fba032188b976c26f2b140e77e
2014000061-JosePortillo <j.portillo.5adb@gmail.com>
2026-04-18 20:03:41 -0600

    Add files via upload
    Ejercicios de C#

---
+++ C#/Ejercicio 1 en Casa.cs
@@ -0,0 +1,47 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+        Console.WriteLine("CALCULADORA FUNCIONES TRIGONOMETRICAS BASICAS");
+        Console.WriteLine("Escriba el valor del: Opuesto, Adyacente e Hipotenusa");
+        double op = Convert.ToDouble(Console.ReadLine());
+        double ad = Convert.ToDouble(Console.ReadLine());
+        double hip = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("\nLista de funciones trigonometricas");
+        Console.WriteLine("1 - Seno");
+        Console.WriteLine("2 - Coseno");
+        Console.WriteLine("3 - Tangente");
+        Console.WriteLine("4 - Salir");
+        Console.WriteLine("Digite el valor segun sea la operacion: [ ]");
+        int opc = Convert.ToInt32(Console.ReadLine());
+
+        string msj = "";
+        double result = 0;
+        switch (opc)
+        {
+            case 1:
+                msj = "\nSENO\nEl seno del angulo es:";
+                result = (op / hip);
+                break;
+            case 2:
+                msj = "\nCOSENO\nEl coseno del angulo es:";
+                result = ad / hip;
+                break;
+            case 3:
+                msj = "\nTANGENTE\nEl tangente del angulo es:";
+                result = op / ad;
+                break;
+            case 4:
+                msj = "Saliendo del sistema...";
+                break;
+            default:
+                msj = "Seleccione un opcion valida";
+                break;
+        }
+        Console.Clear();
+        Console.WriteLine(msj);
+        Console.WriteLine(result);
+    }
+}
---
+++ C#/Ejercicio 5 En Casa.cs
@@ -0,0 +1,60 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        double cant, opc, total, precioIVA , descuento , totalcon ;
+        string forma;
+
+        Console.WriteLine("--- COMPRA DE IMPRESORAS - T&S, S.A de C.A ---");
+        Console.WriteLine("Impresoras de tinta continua c/u Q650 (Sin IVA)");
+        Console.Write("Ingrese la cantidad de impresoras que desea adquirir: ");
+        cant = double.Parse(Console.ReadLine());
+        Console.WriteLine("----------------------------------------------");
+        Console.WriteLine("Seleccione una forma de pago (1-3): ");
+        Console.WriteLine("(Se aplicara un descuento dependiendo de la forma de pago)");
+        Console.WriteLine("(Se le aplicara IVA)");
+        Console.WriteLine("1 - Efectivo - 10%");
+        Console.WriteLine("2 - Tarjeta de credito - 5%");
+        Console.WriteLine("3 - Vale de pago - 15%");
+        opc = double.Parse(Console.ReadLine());
+
+        switch (opc)
+        {
+            case 1:
+                forma = "Efectivo";
+                total = 650 * cant;
+                precioIVA = total * 1.12;
+                descuento = (precioIVA) * (10.0 / 100.0);
+                totalcon = precioIVA - descuento;
+                break;
+
+            case 2:
+                forma = "Tarjeta de credito";
+                total = 650 * cant;
+                precioIVA = total * 1.12;
+                descuento = (precioIVA) * (5.0 / 100.0);
+                totalcon = precioIVA - descuento;
+                break;
+
+            case 3:
+                forma = "Vale de pago";
+                total = 650 * cant;
+                precioIVA = total * 1.12;
+                descuento = (precioIVA) * (15.0 / 100.0);
+                totalcon = precioIVA - descuento;
+                break;
+
+            default:
+                Console.WriteLine("Opción no válida");
+                return;
+        }
+
+        Console.WriteLine("----------------------------------------------");
+        Console.WriteLine("--------------DETALLES DEL PAGO---------------");
+        Console.WriteLine("Cantidad de impresoras a comprar: " + cant);
+        Console.WriteLine("Total sin descuento (con IVA): Q" + precioIVA.ToString("F2"));
+        Console.WriteLine("Forma de pago: " + forma);
+        Console.WriteLine("Descuento realizado: Q" + descuento.ToString("F2"));
+        Console.WriteLine("Total a pagar: Q" + totalcon.ToString("F2"));
+    }
+}
